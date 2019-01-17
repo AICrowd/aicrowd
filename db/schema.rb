84e6c43662445bc8b692d5ffd70593c8c393a8e2
Sphinx <mridulnagpal07@gmail.com>
2019-01-17 16:36:57 +0530

    Assets rendering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_021746) do
+ActiveRecord::Schema.define(version: 2019_01_11_110530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
