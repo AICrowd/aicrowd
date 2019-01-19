ba1f550b6e4a5fcceee7eaef33d1747a0f85a7c8
Arjun Nemani <nemaniarjun@gmail.com>
2019-01-19 18:59:12 +0530

    Render Prizes Breakdown from model
    Add a new migration file for `challenges` model
    Create new concept for `PrizesBreakdown`
    Render new concept wherever needed
    Update seed.rb for prizes
    TODO Tests
    
    Signed-off-by: Arjun Nemani <nemaniarjun@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ if Rails.env == 'development'
   BaseLeaderboard.destroy_all
   ChallengeRound.destroy_all
   Challenge.destroy_all
+  Partner.destroy_all
   Organizer.destroy_all
 
   # Admin
@@ -35,8 +36,8 @@ if Rails.env == 'development'
 
   # Organizers
   Organizer.create!([
-    {id: 21, organizer: "Poznan University of Technology", address: "Laboratory of Intelligent Decision Support Systems", description: "", approved: true, slug: "poznan-university-of-technology", image_file: File.open(Rails.root + "app/assets/images/img/logo-organizer-spotify.svg"), tagline: "Poznan University of Technology", challenge_proposal: "", api_key: "14b4a11368bb0e22134137aa55bf86af", clef_organizer: false},
-    {id: 17, organizer: "SBB CFF FFS", address: "Switzerland", description: "SBB Swiss Federal Railways", approved: true, slug: "sbb-cff-ffs", image_file: File.open(Rails.root + "app/assets/images/img/logo-organizer-spotify.svg"), tagline: "SBB CFF FFS", challenge_proposal: "", api_key: "", clef_organizer: false}
+    {id: 21, organizer: "Poznan University of Technology", address: "Laboratory of Intelligent Decision Support Systems", description: "", approved: true, slug: "poznan-university-of-technology", tagline: "Poznan University of Technology", challenge_proposal: "", api_key: "14b4a11368bb0e22134137aa55bf86af", clef_organizer: false},
+    {id: 17, organizer: "SBB CFF FFS", address: "Switzerland", description: "SBB Swiss Federal Railways", approved: true, slug: "sbb-cff-ffs", tagline: "SBB CFF FFS", challenge_proposal: "", api_key: "", clef_organizer: false}
   ])
 
   member_poznac_ids = [1020, 1050, 1110]
@@ -76,6 +77,10 @@ if Rails.env == 'development'
     {id: 37,
       organizer_id: 21,
       challenge: "Train Schedule Optimisation Challenge",
+      prize_cash: "$1 Million USD",
+      prize_travel: "5",
+      prize_academic: "8",
+      prize_misc: "Some other things",
       status_cd: "running",
       tagline: "Optimizing train schedules ",
       primary_sort_order_cd: "ascending",
@@ -119,6 +124,10 @@ if Rails.env == 'development'
     {id: 30,
       organizer_id: 17,
       challenge: "Visual Doom AI Competition 2018 - Singleplayer Track (1)",
+      prize_cash: "$2 Million USD",
+      prize_travel: "3",
+      prize_academic: "4",
+      prize_misc: "Swag",
       status_cd: "completed",
       tagline: "ViZDoom Reinforcement Learning ",
       primary_sort_order_cd: "ascending",
